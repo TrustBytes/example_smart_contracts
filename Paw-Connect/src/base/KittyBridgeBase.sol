7f882707c0735cbb7b5096fc2daf0ac3909900de
Kerri Rapes <kerri.rapes@gmail.com>
2024-04-29 17:21:16 -0400

    Update and rename PawsBridgeBase.sol to KittyBridgeBase.sol

--- Paw-Connect/src/base/KittyBridgeBase.sol
+++ Paw-Connect/src/base/KittyBridgeBase.sol
@@ -5,15 +5,15 @@ pragma solidity 0.8.19;
 import {IERC20} from
     "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
 
-abstract contract PawsBridgeBase {
-    error PawsBridge__NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
-    error PawsBridge__NothingToWithdraw();
-    error PawsBridge__FailedToWithdrawEth(address owner, address target, uint256 value);
-    error PawsBridge__DestinationChainNotAllowlisted(uint64 destinationChainSelector);
-    error PawsBridge__SourceChainNotAllowlisted(uint64 sourceChainSelector);
-    error PawsBridge__SenderNotAllowlisted(address sender);
-    error PawsBridge__InvalidReceiverAddress();
-    error PawsBridge__NotPawsConnect();
+abstract contract KittyBridgeBase {
+    error KittyBridge__NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
+    error KittyBridge__NothingToWithdraw();
+    error KittyBridge__FailedToWithdrawEth(address owner, address target, uint256 value);
+    error KittyBridge__DestinationChainNotAllowlisted(uint64 destinationChainSelector);
+    error KittyBridge__SourceChainNotAllowlisted(uint64 sourceChainSelector);
+    error KittyBridge__SenderNotAllowlisted(address sender);
+    error KittyBridge__InvalidReceiverAddress();
+    error KittyBridge__NotKittyConnect();
 
     event MessageSent(
         bytes32 indexed messageId,
@@ -31,31 +31,31 @@ abstract contract PawsBridgeBase {
     mapping(uint64 => bool) public allowlistedSourceChains;
     mapping(address => bool) public allowlistedSenders;
     IERC20 internal s_linkToken;
-    address internal PawsConnect;
+    address internal kittyConnect;
 
     modifier onlyAllowlistedDestinationChain(uint64 _destinationChainSelector) {
         if (!allowlistedDestinationChains[_destinationChainSelector]) {
-            revert PawsBridge__DestinationChainNotAllowlisted(_destinationChainSelector);
+            revert KittyBridge__DestinationChainNotAllowlisted(_destinationChainSelector);
         }
         _;
     }
 
     modifier onlyAllowlisted(uint64 _sourceChainSelector, address _sender) {
         if (!allowlistedSourceChains[_sourceChainSelector]) {
-            revert PawsBridge__SourceChainNotAllowlisted(_sourceChainSelector);
+            revert KittyBridge__SourceChainNotAllowlisted(_sourceChainSelector);
         }
-        if (!allowlistedSenders[_sender]) revert PawsBridge__SenderNotAllowlisted(_sender);
+        if (!allowlistedSenders[_sender]) revert KittyBridge__SenderNotAllowlisted(_sender);
         _;
     }
 
     modifier validateReceiver(address _receiver) {
-        if (_receiver == address(0)) revert PawsBridge__InvalidReceiverAddress();
+        if (_receiver == address(0)) revert KittyBridge__InvalidReceiverAddress();
         _;
     }
 
-    modifier onlyPawsConnect() {
-        if (msg.sender != PawsConnect) {
-            revert PawsBridge__NotPawsConnect();
+    modifier onlyKittyConnect() {
+        if (msg.sender != kittyConnect) {
+            revert KittyBridge__NotKittyConnect();
         }
         _;
     }
